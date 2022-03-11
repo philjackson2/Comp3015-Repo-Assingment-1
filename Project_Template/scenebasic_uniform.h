6d82b3771d0f55640e5db5b0035fbde63ab27c28
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-11 12:16:22 +0000

    template

---
+++ Project_Template/scenebasic_uniform.h
@@ -0,0 +1,32 @@
+#ifndef SCENEBASIC_UNIFORM_H
+#define SCENEBASIC_UNIFORM_H
+
+#include "helper/scene.h"
+
+#include <glad/glad.h>
+#include "helper/glslprogram.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+
+class SceneBasic_Uniform : public Scene
+{
+private:
+    GLuint vaoHandle;
+    GLSLProgram prog;
+    float angle;
+    glm::mat4 rotationMatrix;
+
+    void compile();
+
+public:
+    SceneBasic_Uniform();
+
+    void initScene();
+    void update( float t );
+    void render();
+    void resize(int, int);
+};
+
+#endif // SCENEBASIC_UNIFORM_H
